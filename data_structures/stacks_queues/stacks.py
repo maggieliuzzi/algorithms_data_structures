8838733a6e4c041330e5e3a128c58a3c2038844c
Maggie Liuzzi <mrmliuzzi@gmail.com>
2020-05-02 21:26:30 +1000

    Added stack implementation using Python built-in lists

--- data_structures/stacks_queues/stacks.py
+++ data_structures/stacks_queues/stacks.py
@@ -4,12 +4,52 @@ Stacks
 Can be implemented with arrays or linked lists
 """
 
+class Stack:
+    '''
+    Implemented using arrays
+    '''
+    def __init__(self):  # could allow for empty initialisation instead
+        self.list = []
+
+    def peek(self):
+        "Returns top item without removing it from the stack"
+        if len(self.list) == 0:
+            return None
+        return self.list[len(self.list) - 1]
+
+    def push(self, value):
+        "Returns stack with added value at the top"
+        self.list.append(value)  # grows to the right
+        return self
+
+    def pop(self):
+        "Returns top item and removes it from the stack"
+        if len(self.list) == 0:  # or if not self.top
+            return None
+        popped = self.list[len(self.list) - 1]  # required if we want to return popped
+        self.list.pop()
+        return popped
+
+    def is_empty(self):
+        "Returns true if stack is empty and false otherwise"
+        if len(self.list) == 0:
+            return True
+        else:
+            return False
+
+    def traverse(self):
+        return self.list
+    
+
 class Node:
     def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
-
+"""
 class Stack:
+    '''
+    Implemented using Linked Lists
+    '''
     def __init__(self):  # could allow for empty initialisation instead
         self.bottom = Node()
         self.top = self.bottom
@@ -49,53 +89,55 @@ class Stack:
         else:
             return False
 
-    def traverse(self):
+    def traverse(self):  # traversing not encouraged with stacks, O(n)
         values = []
         curr = self.top
         while curr:
             values.append(curr.value)
             curr = curr.next
         return values
-
+"""
 
 stack = Stack()
-print(stack.length)
+print(stack.peek())
+# print(stack.length)
 print(stack.is_empty())
 stack.push(1)
-print(stack.length)
+# print(stack.length)
 print(stack.is_empty())
+print(stack.traverse())
 print(" ")
-print(stack.peek().value)
-print(stack.top.value)
-print(stack.bottom.value)
+print(stack.peek())  # print(stack.peek().value)
+# print(stack.top.value)
+# print(stack.bottom.value)
 print(stack.traverse())
 print(" ")
 stack.push(2)
-print(stack.length)
-print(stack.peek().value)
-print(stack.top.value)
-print(stack.bottom.value)
+# print(stack.length)
+print(stack.peek())  # print(stack.peek().value)
+# print(stack.top.value)
+# print(stack.bottom.value)
 print(stack.traverse())
 print(" ")
 stack.push("google")
-print(stack.length)
-print(stack.peek().value)
-print(stack.top.value)
-print(stack.bottom.value)
+# print(stack.length)
+print(stack.peek())  # print(stack.peek().value)
+# print(stack.top.value)
+# print(stack.bottom.value)
 print(stack.traverse())
 print(" ")
 stack.push(3)
-print(stack.length)
-print(stack.peek().value)
-print(stack.top.value)
-print(stack.top.next.value)
-print(stack.bottom.value)
+# print(stack.length)
+print(stack.peek())  # print(stack.peek().value)
+# print(stack.top.value)
+# print(stack.top.next.value)
+# print(stack.bottom.value)
 print(stack.traverse())
 print(" ")
 print(stack.pop())
-print(stack.length)
-print(stack.top.value)
-print(stack.bottom.value)
+# print(stack.length)
+# print(stack.top.value)
+# print(stack.bottom.value)
 print(stack.traverse())
 stack.pop()
 stack.pop()
