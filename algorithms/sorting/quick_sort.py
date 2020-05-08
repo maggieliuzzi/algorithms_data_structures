73251696ee49959b5ba6beceb541ab01d5bb2859
Maggie Liuzzi <mrmliuzzi@gmail.com>
2020-05-08 21:29:32 +1000

    Modified Quick Sort

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -18,24 +18,18 @@ Worst case: O(n^2) (when the pivot chosen happens to be the smallest or largest 
 # Pivot element is placed at its correct position in sorted array
 # All elements smaller than pivot are placed to the left of the pivot one and all greater ones to the right
 def partition(arr, low, high):   #  Eg. [3, 2, 5, 1, 87, 3, 21, 2.5, 0, -1, 7]
-    print("\n\t--> ", arr)
-    i = low - 1           # index of smaller element 
-    pivot = arr[high]     # pivot 
-    print("\n\tPivot: ", pivot, "\n")
-  
-    for j in range(low, high): 
-        # if current element is smaller than or equal to pivot 
-        if arr[j] <= pivot: 
-            i += 1  # incrementing index of smaller element
-            print(i, j)
-            print(arr[i], arr[j])
-            arr[i], arr[j] = arr[j], arr[i] 
-            print(arr[i], arr[j]); print(" ")
-    
-    print(list)
-    arr[i+1], arr[high] = arr[high], arr[i+1] 
-    print(list)
-    return i + 1  # new position of the pivot
+    pivot = arr[high]  # pivot value
+    pi = low  # partition index 
+     
+    for i in range(low, high): 
+        if arr[i] <= pivot:  # if current element is smaller than or equal to pivot
+            print("\t", arr, pi, i)
+            if pi != i:
+                arr[pi], arr[i] = arr[i], arr[pi]
+            pi += 1  # index of smaller element
+
+    arr[pi], arr[high] = arr[high], arr[pi] 
+    return pi  # position of pivot in sorted list
   
   
 def quickSort(arr, low, high):
@@ -43,17 +37,14 @@ def quickSort(arr, low, high):
     low: start index
     high: end index
     """
-    if low < high: 
-  
-        # pi is partitioning index, arr[p] is now at the right place 
-        pi = partition(arr, low, high) 
-  
-        # separately sort elements before partition and after partition 
-        quickSort(arr, low, pi - 1) 
-        quickSort(arr, pi + 1, high) 
-  
+    if low < high:
+
+        pi = partition(arr, low, high)  # partitioning index. pivot now correctly placed at arr[pi]
+
+        quickSort(arr, low, pi - 1)  # sort elements before pivot
+        quickSort(arr, pi + 1, high)  # sort elements after pivot 
+
 
 list = [3, 2, 5, 1, 87, 3, 21, 2.5, 0, -1, 7]
-n = len(list) 
-quickSort(list, 0, n-1)
+quickSort(list, 0, len(list) - 1)
 print(list)
